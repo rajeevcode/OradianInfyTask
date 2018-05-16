b01f2866e8514dbd4bfdddfbbf8fa27a7e2a8ea5
Rajeev Kumar <rajeev.code@gmail.com>
2018-05-16 17:34:50 +0700

    initial commit

--- src/test/java/com/infy/qa/testcases/LoginPageTest.java
+++ src/test/java/com/infy/qa/testcases/LoginPageTest.java
@@ -12,9 +12,9 @@ public class LoginPageTest extends TestBase {
     LoginPage loginPage;
     HomePage homePage;
 
-    public LoginPageTest() {
-        super ( );
-    }
+//    public LoginPageTest() {
+//        super ( );
+//    }
 
     @BeforeMethod
     public void setUp() {
