845eadcdf699cfe2f52fb68fe3d161ed3f83ed40
Rajeev Kumar <rajeev.code@gmail.com>
2018-05-18 11:32:19 +0700

    Update TestUtil.java

--- src/main/java/com/infy/qa/util/TestUtil.java
+++ src/main/java/com/infy/qa/util/TestUtil.java
@@ -13,12 +13,6 @@ public class TestUtil extends TestBase {
     public static long PAGE_LOAD_TIMEOUT = 20;
     public static long IMPLICIT_WAIT = 20;
 
-    // created common method for frames
-//    public void switchToFrame() {
-//        driver.switchTo ( ).frame ( "" );
-//    }
-//}
-
     public static void takeScreenshotAtEndOfTest() throws IOException {
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs ( OutputType.FILE );
         String currentDir = System.getProperty ( "user.dir" );
